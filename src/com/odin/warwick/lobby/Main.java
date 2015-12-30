80c7db24654eefa099aea7bedc8844dc6cfcdfa9
toxic906906 <scopes.bobby@gmail.com>
2015-12-30 00:06:05 -0500

    Add plugin.yml

---
+++ src/com/odin/warwick/lobby/Main.java
@@ -0,0 +1,14 @@
+package com.odin.warwick.lobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    // Called when plugin is first enabled.
+    @Override
+    public void onEnable() {
+    }
+    // Called when plugin is disabled.
+    @Override
+    public void onDisable() {
+    }
+}
